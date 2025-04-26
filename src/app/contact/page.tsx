9c6ff67573ee1f19e1999c739c9ce37c176a48c5
hyeoksu1234 <114197095+hyeoksu1234@users.noreply.github.com>
2025-04-26 20:54:09 +0900

    전화하기 버튼 텍스트 수정

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -171,7 +171,7 @@ export default function ContactPage() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.948.684l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.042 11.042 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              전화하기 (010-3406-5414)
+              전화하기
             </Link>
             <Link href="mailto:leeyoon@ma-cc.co.kr" className="w-full md:w-auto inline-flex items-center justify-center bg-transparent text-white border border-white px-6 md:px-8 py-3 rounded-lg font-semibold hover:bg-white hover:text-blue-600 transition">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
