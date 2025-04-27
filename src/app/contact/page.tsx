d9df43ead01c7c09274885ec158d5719e6925470
hyeoksu1234 <114197095+hyeoksu1234@users.noreply.github.com>
2025-04-27 23:34:45 +0900

    모바일에서 키워드 요소들을 한 줄로 표시되도록 수정

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -40,20 +40,20 @@ export default function ContactPage() {
           <p className="text-base md:text-lg">
             무료 초기 상담을 통해 귀하의 목표와 상황에 맞는 최적의 서비스를 안내해 드립니다.
           </p>
-          <div className="flex flex-col md:flex-row justify-center md:space-x-6 space-y-3 md:space-y-0 mt-6 md:mt-8">
-            <div className="flex items-center justify-center text-blue-600">
+          <div className="flex flex-row flex-wrap justify-center space-x-3 space-y-0 mt-6 md:mt-8">
+            <div className="flex items-center justify-center text-blue-600 ml-3 mt-3 md:mt-0">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
               <span>맞춤형 솔루션</span>
             </div>
-            <div className="flex items-center justify-center text-blue-600">
+            <div className="flex items-center justify-center text-blue-600 mt-3 md:mt-0">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
               <span>전문가 코칭</span>
             </div>
-            <div className="flex items-center justify-center text-blue-600">
+            <div className="flex items-center justify-center text-blue-600 mt-3 md:mt-0">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
               </svg>
